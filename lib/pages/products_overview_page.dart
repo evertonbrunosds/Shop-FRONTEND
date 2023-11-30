01bcc255b006ef5bd67fc2ac7d0184074bd77db4
evertonbrunosds <evertonbrunosds@gmail.com>
2023-11-29 23:42:53 -0300

    v0.2 - Extraindo ProductGrid

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product_list.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    final loadedProducts = Provider.of<ProductList>(context).items;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -22,18 +18,7 @@ class ProductsOverviewPage extends StatelessWidget {
       ),
       body: Container(
         color: Theme.of(context).colorScheme.primary.withOpacity(0.3),
-        child: GridView.builder(
-          padding: const EdgeInsets.all(10),
-          itemCount: dummyProducts.length,
-          itemBuilder: (_, index) =>
-              ProductItem(product: loadedProducts[index]),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-        ),
+        child: const ProductGrid(),
       ),
     );
   }
