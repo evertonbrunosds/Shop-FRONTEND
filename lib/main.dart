9c6021e19bdf23f49607c218e80fcf516af0dbcb
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-01 00:07:01 -0300

    v0.7 - Aplicação do MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models/cart.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
 import 'package:shop/utils/application_theme.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,8 +17,15 @@ class MyApp extends StatelessWidget {
     final applicationTheme = ApplicationTheme(
       theme: ThemeData(fontFamily: 'Lato'),
     );
-    return ChangeNotifierProvider.value(
-      value: ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: ProductList(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        )
+      ],
       child: MaterialApp(
         theme: applicationTheme.configureTheme(color: Colors.deepPurple),
         home: const ProductsOverviewPage(),
