053fda2b7d6aad3ae0d1b9de598964ec46ae5785
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-03 21:56:28 -0300

    v1.7 - adicionando provedor de pedidos

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -23,6 +24,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider.value(
           value: Cart(),
+        ),
+        ChangeNotifierProvider.value(
+          value: OrderList(),
         )
       ],
       child: MaterialApp(
