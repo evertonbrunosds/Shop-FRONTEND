b7ca1d448bd24958a3b27938dd4f13717a9ba366
evertonbrunosds <evertonbrunosds@gmail.com>
2023-12-06 00:42:58 -0300

    v3.1 - exclusão de produto con confirmação

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
@@ -7,6 +9,55 @@ import 'package:shop/utils/app_routes.dart';
 class ProductItem extends StatelessWidget {
   final Product product;
   const ProductItem({super.key, required this.product});
+
+  Future<bool?> confirm(final BuildContext context, {required String name}) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return showDialog<bool>(
+      barrierColor: Colors.transparent,
+      context: context,
+      builder: (_) => BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 15, sigmaY: 15),
+        child: AlertDialog(
+          backgroundColor: colorScheme.background,
+          surfaceTintColor: Colors.transparent,
+          title: Text(
+            'Tem certeza?',
+            style: TextStyle(color: colorScheme.secondary),
+          ),
+          content: Text(
+            'Quer remover o produto "$name" da lista de produtos?',
+            style: TextStyle(color: colorScheme.secondary),
+          ),
+          actions: [
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: colorScheme.secondary,
+              ),
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text(
+                'Não',
+                style: TextStyle(color: colorScheme.primary),
+              ),
+            ),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(
+                  color: colorScheme.secondary,
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text(
+                'Sim',
+                style: TextStyle(color: colorScheme.secondary),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(final BuildContext context) {
@@ -27,10 +78,16 @@ class ProductItem extends StatelessWidget {
               icon: const Icon(Icons.edit),
             ),
             IconButton(
-              onPressed: () => Provider.of<ProductList>(
-                context,
-                listen: false,
-              ).remove(product),
+              onPressed: () => confirm(context, name: product.name).then(
+                (result) {
+                  if (result ?? false) {
+                    Provider.of<ProductList>(
+                      context,
+                      listen: false,
+                    ).remove(product);
+                  }
+                },
+              ),
               icon: const Icon(Icons.delete),
             ),
           ],
